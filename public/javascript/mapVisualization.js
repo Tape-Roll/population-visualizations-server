839c35d5d279ce664746e2374ec8eebf1066d039
Jim Harris <jharris9456@gmail.com>
2018-04-08 15:18:10 -0400

    refactor mapVisualizations

--- public/javascript/mapVisualization.js
+++ public/javascript/mapVisualization.js
@@ -1,77 +1,88 @@
-(function() {
+var mapVisualization = (function() {
     'use strict';
-    var svg = d3.select("svg");
-    var width = +svg.attr("width");
-    var height = +svg.attr("height");
-	var statisticName = "Test Statistic";
 
-    var mobilityStatistics = d3.map();
+    var renderKeyOnSVG = function(statisticName, color) {
+        var svg = d3.select("svg");
+        var width = +svg.attr("width");
+        var height = +svg.attr("height");
+        var x = d3.scaleLinear()
+            .domain([1, 10])
+            .rangeRound([600, 860]);
 
-    var path = d3.geoPath();
+        var g = svg.append("g")
+            .attr("class", "key")
+            .attr("transform", "translate(0,40)");
 
-	var x = d3.scaleLinear()
-		.domain([1, 10])
-		.rangeRound([600, 860]);
+        g.selectAll("rect")
+            .data(color.range().map(function(d) {
+                d = color.invertExtent(d);
+                if (d[0] == null) d[0] = x.domain()[0];
+                if (d[1] == null) d[1] = x.domain()[1];
+                return d;
+            }))
+            .enter().append("rect")
+                .attr("height", 8)
+                .attr("x", function(d) { return x(d[0]); })
+                .attr("width", function(d) { return x(d[1]) - x(d[0]); })
+                .attr("fill", function(d) { return color(d[0]); });
 
-	var color = d3.scaleThreshold()
-		.domain(d3.range(2, 10))
-		.range(d3.schemeBlues[9]);
+        g.append("text")
+            .attr("class", "caption")
+            .attr("x", x.range()[0])
+            .attr("y", -6)
+            .attr("fill", "#000")
+            .attr("text-anchor", "start")
+            .attr("font-weight", "bold")
+            .text(statisticName);
 
-	var g = svg.append("g")
-		.attr("class", "key")
-		.attr("transform", "translate(0,40)");
+        g.call(d3.axisBottom(x)
+            .tickSize(13)
+            .tickFormat(function(x, i) { return i ? x : x + "%"; })
+            .tickValues(color.domain()))
+            .select(".domain")
+            .remove();
+    };
 
-	g.selectAll("rect")
-	  .data(color.range().map(function(d) {
-		  d = color.invertExtent(d);
-		  if (d[0] == null) d[0] = x.domain()[0];
-		  if (d[1] == null) d[1] = x.domain()[1];
-		  return d;
-		}))
-	  .enter().append("rect")
-		.attr("height", 8)
-		.attr("x", function(d) { return x(d[0]); })
-		.attr("width", function(d) { return x(d[1]) - x(d[0]); })
-		.attr("fill", function(d) { return color(d[0]); });
+    var renderMapOnSVG = function(topoJSON, color) {
+        var svg = d3.select("svg");
+        var width = +svg.attr("width");
+        var height = +svg.attr("height");
+        var path = d3.geoPath();
 
-	g.append("text")
-		.attr("class", "caption")
-		.attr("x", x.range()[0])
-		.attr("y", -6)
-		.attr("fill", "#000")
-		.attr("text-anchor", "start")
-		.attr("font-weight", "bold")
-		.text(statisticName);
+        svg.append("g")
+            .attr("class", "counties")
+            .selectAll("path")
+            .data(topojson.feature(topoJSON, topoJSON.objects.counties).features)
+            .enter().append("path")
+                .attr("fill", function(d) { return color(d.rate = 8); })
+                .attr("d", path)
+            .append("title")
+                .text(function(d) { return d.rate + "%"; });
 
-	g.call(d3.axisBottom(x)
-		.tickSize(13)
-		.tickFormat(function(x, i) { return i ? x : x + "%"; })
-		.tickValues(color.domain()))
-	  .select(".domain")
-		.remove();
+        svg.append("g")
+            .attr("class", "states")
+            .selectAll("path")
+            .data(topojson.feature(topoJSON, topoJSON.objects.states).features)
+            .enter().append("path")
+                .attr("d", path)
+    };
 
-	d3.queue()
-		.defer(d3.json, "https://d3js.org/us-10m.v1.json")
-		.await(ready);
+    var requestUSTopoJSON = function(callback) {
+        d3.queue()
+            .defer(d3.json, "https://d3js.org/us-10m.v1.json")
+            .await(callback);
+    };
 
-	function ready(error, us) {
-	  if (error) throw error;
+    var resetSVG = function() {
+        var svg = d3.select("svg");
+        svg.html("");
+    }
 
-	  svg.append("g")
-		  .attr("class", "counties")
-		.selectAll("path")
-		.data(topojson.feature(us, us.objects.counties).features)
-		.enter().append("path")
-		  .attr("fill", function(d) { return color(d.rate = 5); })
-		  .attr("d", path)
-		.append("title")
-		  .text(function(d) { return d.rate + "%"; });
+    return  {
+        requestUSTopoJSON,
+        renderMapOnSVG,
+        renderKeyOnSVG,
+        resetSVG
+    };
 
-	  svg.append("g")
-		  .attr("class", "states")
-		.selectAll("path")
-		.data(topojson.feature(us, us.objects.states).features)
-		.enter().append("path")
-		  .attr("d", path)
-	}
 }())
