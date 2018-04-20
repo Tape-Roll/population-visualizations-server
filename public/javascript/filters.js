3396e258bb9f2a6cac1a96c791604aa646327726
crschriever <crschriever@gmail.com>
2018-04-20 16:25:09 -0400

    filters hide now

--- public/javascript/filters.js
+++ public/javascript/filters.js
@@ -1,9 +1,112 @@
+var $dropDown1;
+var $dropDown2;
+var $dropDown3;
+var $run;
+var $clear;
+
 $(function() {
-    var $dropDown1 = getDropDown("#drop1");
+    $dropDown1 = getDropDown("#drop1");
+    $dropDown2 = getDropDown("#drop2");
+    $dropDown3 = getDropDown("#drop3");
+    $run = $("#run-button");
+    $clear = $("#clear-button");
+
+    $clear.on("click", clear);
+    $run.on("click", run);
+
+    hideStuff();
+
     Object.keys(filters).forEach(function(key) {
-        $dropDown1.menu.prepend('<div class="dropdown-item">' + key + "</div>");
+        var $option = $('<div class="dropdown-item">' + key + "</div>");
+        $option.obj = filters[key];
+
+        // Handle menu1 clicks
+        $option.on("click", function(event) {
+            $dropDown1.button.text(key);
+            $dropDown2.menu.empty();
+            $dropDown2.button.text("Filter");
+
+            if ($option.obj.categories !== undefined) {
+                console.log("2 menus");
+                Object.keys($option.obj.categories).forEach(function(key) {
+                    var $subOption = $('<div class="dropdown-item">' + key + "</div>");
+                    $subOption.obj = $option.obj.categories[key];
+
+                    //Handle menu2 clicks
+                    $subOption.on("click", function(event) {
+                        $dropDown2.button.text(key);
+                        finalCategory();
+                    });
+
+                    // Append menu 2
+                    $dropDown2.menu.append($subOption);
+                });
+
+                $dropDown2.css("visibility", "visible");
+            } else if ($option.obj.contains !== undefined) {
+                console.log("3 menus");
+                $option.obj.contains.forEach(function(key) {
+                    var $subOption = $('<div class="dropdown-item">' + key + "</div>");
+                    $subOption.obj = filters["total_" + key];
+
+                    //Handle menu2 clicks
+                    $subOption.on("click", function(event) {
+                        $dropDown2.button.text(key);
+                        $dropDown3.menu.empty();
+                        $dropDown3.button.text("Filter");
+
+                        Object.keys($subOption.obj.categories).forEach(function(key) {
+                            console.log("3rd added");
+                            var $finalOption = $('<div class="dropdown-item">' + key + "</div>");
+                            $finalOption.obj = $subOption.obj.categories[key];
+
+                            //Handle menu3 clicks
+                            $finalOption.on("click", function(event) {
+                                $dropDown3.button.text(key);
+                                finalCategory();
+                            });
+
+                            // Append menu 3
+                            $dropDown3.menu.append($finalOption);
+                        });
+                        $dropDown3.css("visibility", "visible");
+                    });
+
+                    // Append menu 2
+                    $dropDown2.menu.append($subOption);
+                });
+
+                $dropDown2.css("visibility", "visible");
+            } else {
+                $dropDown2.css("visibility", "hidden");
+                $dropDown3.css("visibility", "hidden");
+                finalCategory();
+            }
+        });
+
+        // Append menu 1
+        $dropDown1.menu.append($option);
     });
 });
+
+function finalCategory() {
+    $run.removeClass("disabled");
+}
+
+function clear() {
+    hideStuff();
+    $dropDown1.button.text("Filter");
+}
+
+function run() {
+    console.log("Todo");
+}
+
+function hideStuff() {
+    $dropDown2.css("visibility", "hidden");
+    $dropDown3.css("visibility", "hidden");
+    $run.addClass("disabled");
+}
 
 function getDropDown(id) {
     var drop = $(id);
