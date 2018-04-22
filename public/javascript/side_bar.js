676ed16a40e660c2c15434320f4ffe3b5205ac11
crschriever <crschriever@gmail.com>
2018-04-22 11:18:35 -0400

    Menu will hide and show itself when clicked

--- public/javascript/side_bar.js
+++ public/javascript/side_bar.js
@@ -1,13 +1,19 @@
-var side_bar = (function() {
+// Called whenever jquery is ready
+$(function() {
+    var $side_bar_cont = $(".side-bar-cont");
+    var $side_bar = $side_bar_cont.children(".side-bar");
+    $side_bar.shown = true;
+    var $close_button = $("#close-button");
 
-    var updateTitle = function() {
-    }
-
-    var updateTable = function() {
-    }
-
-    return {
-        updateTitle,
-        updateTable
-    }
-}());
+    $close_button.on("click", function(event) {
+        if ($side_bar.shown) {
+            var pos = $side_bar.width() - $close_button.outerWidth();
+            $side_bar_cont.css("left", -pos + "px");
+            $close_button.html(' <i class="fas fa-angle-double-right"></i>');
+        } else {
+            $side_bar_cont.css("left", 0 + "px");
+            $close_button.html(' <i class="fas fa-angle-double-left"></i>');
+        }
+        $side_bar.shown = !$side_bar.shown;
+    });
+});
